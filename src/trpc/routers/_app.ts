d72492db7f0f8c7cfa81b21f1882d1345ab8707a
Muhammad Saad <160317082+sahersaleem@users.noreply.github.com>
2025-11-23 16:33:34 +0500

    07-AI-providers

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,11 +2,27 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 import { inngest } from "@/inngest/client";
+import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
 
 export const appRouter = createTRPCRouter({
+  
+  //use AI function calling thriugh inngest.
+  useAI: protectedProcedure.mutation(async ({ ctx }) => {
+    await inngest.send({
+      name: "execute.ai",
+    });
+     return { success: true, message: "JOb QUEUED" };
+  }),
+
+
+  //returning workflow though protected procedure
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
   }),
+
+
+  //creating workflow thorough inngesr hello world function on succes return job queued
   createWorkflow: protectedProcedure.mutation(async ({ ctx }) => {
     await inngest.send({
       name: "test/hello.world",
@@ -15,7 +31,7 @@ export const appRouter = createTRPCRouter({
       },
     });
 
-    return { success: true , message:"JOb QUEUED"}; 
+    return { success: true, message: "JOb QUEUED" };
   }),
 });
 // export type definition of API
